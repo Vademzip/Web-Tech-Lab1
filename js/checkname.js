f7fb1dc318dbfd5ffc834b8d511a3cb7218f36e5
Vademzip <vadim.sardarov@yandex.ru>
2022-11-04 16:54:06 +0300

    Итоговая работа
    Теперь точно.

--- js/checkname.js
+++ js/checkname.js
@@ -1,8 +1,14 @@
 let firstName = document.getElementById("userName");
 let errors = document.getElementById("name-error")
+let login = document.getElementById("login2");
+let logerr = document.getElementById("login-error")
+let pass1 = document.getElementById("password2")
+let paserr = document.getElementById("password-error")
+let pass2 = document.getElementById("passwordRewrite")
+let paserr2 = document.getElementById("password2-error")
+console.log(paserr2)
 
-
-firstName.addEventListener('focus',() =>{
+firstName.addEventListener('input',() =>{
     if (firstName.value.length <= 3){
         errors.classList.add("on")
     }
@@ -12,13 +18,47 @@ firstName.addEventListener('focus',() =>{
     }
 })
 
-firstName.addEventListener('blur',() =>{
-    if (firstName.value.length <= 3){
-        errors.classList.add("on")
+login.addEventListener('input',() =>{
+    if (login.value.length <= 8){
+        logerr.classList.add("on")
     }
     else
     {
-        errors.classList.remove("on")
+        logerr.classList.remove("on")
     }
 })
+
+pass1.addEventListener('input',() =>{
+    if (pass1.value.length <= 8){
+        paserr.classList.add("on")
+    }
+    else
+    {
+        paserr.classList.remove("on")
+    }
+})
+
+pass2.addEventListener('input',() =>{
+    if (pass1.value != pass2.value){
+        paserr2.classList.add("on")
+    }
+    else
+    {
+        paserr2.classList.remove("on")
+    }
+})
+
+
+
+
+
+// firstName.addEventListener('blur',() =>{
+//     if (firstName.value.length <= 3){
+//         errors.classList.add("on")
+//     }
+//     else
+//     {
+//         errors.classList.remove("on")
+//     }
+// })
 
